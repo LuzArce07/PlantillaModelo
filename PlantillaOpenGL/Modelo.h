089fe0f0b250db88944a3410ca95e355219f37a6
Luz Arelí Arce Contreras <luzareli_7abril@hotmail.com>
2019-04-01 11:50:46 -0700

    glm

--- PlantillaOpenGL/Modelo.h
+++ PlantillaOpenGL/Modelo.h
@@ -21,14 +21,20 @@ class Modelo {
 public:
 
 	vector<Vertice> vertices;
+
+	mat4 transformaciones;
+
 	Shader *shader;
 	GLuint vertexArrayID;
 	GLuint bufferID;
 
+	GLuint transformacionesID;
+
+
 
 	Modelo();
 
-	void inicializarVertexArray(GLuint posicionID, GLuint colorID);
+	void inicializarVertexArray(GLuint posicionID, GLuint colorID, GLuint transformacionesID);
 
 	void dibujar(GLenum modoDibujo);
 		
